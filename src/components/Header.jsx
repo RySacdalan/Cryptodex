13f6f6973eddebcfed98392ec8b8e8d7d20b40d8
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-06-24 11:24:52 +0800

    Add styling to header input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css';
 const Header = ({handleChange}) => {
   return (
     <div className="header__container">
-      <h1 className="header__title">CryptoDex</h1>
+      <h1 className="header__title"><p>Crypto</p>Dex</h1>
       <div className="header__search">
         <input className="header__input" type="text" placeholder="Search crypto" onChange={handleChange} />
-        <button type="submit">Search</button>
+        <button type="submit"><ion-icon name="search-sharp"></ion-icon></button>
       </div>
     </div>
   );
